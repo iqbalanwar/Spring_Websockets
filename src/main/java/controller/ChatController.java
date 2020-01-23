95decbbdb91f1c7e13c2e0755f10b9a92f62f581
Iqbal Anwar <iqbalaanwar@gmail.com>
2020-01-23 13:32:10 -0500

    Created controller and chatmessage model to finish server

---
+++ src/main/java/controller/ChatController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    // Will help us map the same message from client to server
+    @MessageMapping("/chat.register")
+    // Will specify the queue: The request channel and response channel
+    // based on the URL
+    // Note that this also uses /topic endpoint, because it should be the same
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+        // get the username of the chat sender this way:
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public") // This is the same request channel and response channel
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
